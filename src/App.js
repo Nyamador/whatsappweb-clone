26eb9acd8ea5b4913649a8b6e49e4b7dce7d3b1c
Nyamador <dselasi4u@gmail.com>
2020-08-19 23:12:00 +0000

    Added Action Menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import SideBar from './components/SideBar';
 // import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="web">
+			<SideBar />
+		</div>
+	);
 }
 
 export default App;
